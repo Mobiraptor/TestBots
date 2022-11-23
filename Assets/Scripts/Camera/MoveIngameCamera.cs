782babb19443098abf590822b1892c50804b7094
Mobiraptor <rubick208@gmail.com>
2022-11-23 13:45:00 +0600

    feat: camera movement
    Прикрутил скрипт с другого своего проекта, чтобы можно было двигать камеру в игре

--- Assets/Scripts/Camera/MoveIngameCamera.cs
+++ Assets/Scripts/Camera/MoveIngameCamera.cs
@@ -4,14 +4,58 @@ using UnityEngine;
 
 public class MoveIngameCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float axisX;
+    private float axisY;
+    private float maxX = 90;
+    //private float minX = -40;
+    private float cameraSpeed = 1.4f;
 
+
+    private void RotateCamera()
+    {
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            axisY += Input.GetAxis("Mouse X") * cameraSpeed;
+        }
+        if (Input.GetAxis("Mouse Y") != 0)
+        {
+            axisX += Input.GetAxis("Mouse Y") * cameraSpeed;
+        }
+        if (axisX > maxX)
+        {
+            axisX = maxX;
+        }
+        if (axisX < -maxX)
+        {
+            axisX = -maxX;
+        }
+
+        GetComponent<Camera>().transform.rotation = Quaternion.Euler(-axisX, axisY, 0);
+
+
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            GetComponent<Transform>().position += Vector3.forward;
+            GetComponent<Transform>().position += gameObject.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Transform>().position -= gameObject.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Transform>().position -= gameObject.transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Transform>().position += gameObject.transform.right;
+        }
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            RotateCamera();
         }
     }
 }
